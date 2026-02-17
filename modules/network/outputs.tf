ecc19be0b297345e53ef125911bf3d7e123887d5
JessicaApBueno <buenojessicaaparecida@gmail.com>
2026-02-16 22:09:59 -0300

    fix: add missing outputs to network module

--- modules/network/outputs.tf
+++ modules/network/outputs.tf
@@ -1,7 +1,14 @@
+output "vpc_id" {
+  description = "ID da VPC criada no módulo de rede"
+  value       = aws_vpc.main.id
+}
+
 output "subnet_id" {
-  value = aws_subnet.public.id
+  description = "ID da Subnet criada no módulo de rede"
+  value       = aws_subnet.public.id
 }
 
 output "security_group_id" {
-  value = aws_security_group.allow_web.id
+  description = "ID do Security Group criado no módulo de rede"
+  value       = aws_security_group.allow_web.id
 }
